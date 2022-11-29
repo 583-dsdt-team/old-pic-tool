c77a208e924c8a621943dbb308d58e41baca2cab
breon haskett <breonh@uw.edu>
2022-11-28 18:47:26 -0800

    initial detection function and example

---
+++ detection/detection_function.py
@@ -0,0 +1,27 @@
+"""
+Module defines the color detection function
+"""
+
+import numpy
+import pandas
+
+import opencv
+
+rgb_df = pandas.read_csv('~/data/monk_scales_rgb.csv')
+bw_df = pandas.read_csv('~/data/monk_scales_bw.csv')
+
+def complexion_detection(image_path, rounding_places=2, grayscale=False):
+"""
+Uses the inRange function in opencv 
+Detects % of colors from ellis monk scale in image
+"""
+    filler = []
+    temp_image = cv2.imread(image_path)
+    for index in range(rgb_df.shape[0]):
+        upper = numpy.uint8([rgb_df['upper_b'][index], rgb_df['upper_g'][index], rgb_df['upper_r'][index]])
+        lower = numpy.uint8([rgb_df['lower_b'][index], rgb_df['lower_g'][index], rgb_df['lower_r'][index]])
+        mask = cv2.inRange(temp_image, lower, upper)
+        cur_val = (mask > 0).mean()
+        rounded_val = round(cur_val, rounding_places)
+        filler.append(rounded_val)
+    return(filler)
