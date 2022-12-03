7ba7aaff9952bfb7c0b2b724d1b68f2b404a5d9e
breon haskett <breonh@uw.edu>
2022-12-03 15:53:37 -0800

    update detect function and start the dunder init for the detector

--- detection/detection_function.py
+++ detection/detection_function.py
@@ -17,11 +17,21 @@ Detects % of colors from ellis monk scale in image
 """
     filler = []
     temp_image = cv2.imread(image_path)
-    for index in range(rgb_df.shape[0]):
-        upper = numpy.uint8([rgb_df['upper_b'][index], rgb_df['upper_g'][index], rgb_df['upper_r'][index]])
-        lower = numpy.uint8([rgb_df['lower_b'][index], rgb_df['lower_g'][index], rgb_df['lower_r'][index]])
-        mask = cv2.inRange(temp_image, lower, upper)
-        cur_val = (mask > 0).mean()
-        rounded_val = round(cur_val, rounding_places)
-        filler.append(rounded_val)
+    
+    if grayscale == False:
+        for index in range(rgb_df.shape[0]):
+            upper = numpy.uint8([rgb_df['upper_b'][index], rgb_df['upper_g'][index], rgb_df['upper_r'][index]])
+            lower = numpy.uint8([rgb_df['lower_b'][index], rgb_df['lower_g'][index], rgb_df['lower_r'][index]])
+            mask = cv2.inRange(temp_image, lower, upper)
+            cur_val = (mask > 0).mean()
+            rounded_val = round(cur_val, rounding_places)
+            filler.append(rounded_val)
+    elif grayscale == True:
+        for index in range(bw_df.shape[0]):
+            upper = numpy.uint8([bw_df['upper_b'][index], bw_df['upper_g'][index], bw_df['upper_r'][index]])
+            lower = numpy.uint8([bw_df['lower_b'][index], bw_df['lower_g'][index], bw_df['lower_r'][index]])
+            mask = cv2.inRange(temp_image, lower, upper)
+            cur_val = (mask > 0).mean()
+            rounded_val = round(cur_val, rounding_places)
+            filler.append(rounded_val)
     return(filler)
