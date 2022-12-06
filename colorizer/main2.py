943b00c3a7e20a9167ff8f7b75869a7fac4cd7a3
David Coomes <dmcoomes@gmail.com>
2022-12-06 08:27:41 -0800

    colorized grayscale

--- colorizer/main2.py
+++ colorizer/main2.py
@@ -6,35 +6,13 @@ which will include the processed color images.
 """
 
 import os.path
-import glob
-
 from colorizer import *
 
-# DMC - added 'colorizer' to path name
-prototxt = r'colorizer/model/colorization_deploy_v2.prototxt'
-model = r'colorizer/model/colorization_release_v2.caffemodel'
-points = r'colorizer/model/pts_in_hull.npy'
-points = os.path.join(os.path.dirname(__name__), points)        # DMC changing "__file__" to "__name__"
-prototxt = os.path.join(os.path.dirname(__name__), prototxt)
-model = os.path.join(os.path.dirname(__name__), model)
 
-
-# First, get list of files in input folder
-def listdir_nohidden(path):
-    for f in os.listdir(path):
-        if not f.startswith('.'):
-            yield(f)
-
-input_folder = listdir_nohidden("./colorizer/input")
-
-input_folder = os.listdir("./colorizer/input")
-
-def listdir_nohidden(path):
-    return glob.glob(os.path.join(path, '*'))
-    
-
-
+# get list of files in input folder
 input_folder = [f for f in os.listdir("./colorizer/input") if not f.startswith('.')]
+
+# Colorize each file in loop in input folder and save in output folder
 for photo in input_folder:
     img = "./colorizer/input/" + photo
     output = "./colorizer/output/" + photo.rsplit('.', 1)[0] + "_colorized." + photo.rsplit('.', 1)[1]
@@ -43,7 +21,7 @@ for photo in input_folder:
     cv2.imwrite(output, colorized)
 
 
-["./colorizer/input/" + s for s in input_folder]
+["./colorizer/input/" + s for s in input_folder]g
 
 input_folder[0]
 
