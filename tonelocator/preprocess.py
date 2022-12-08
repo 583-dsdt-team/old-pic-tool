04bb6cc0cc463dae20e07f6200e640169e8365df
breon haskett <breonh@uw.edu>
2022-12-08 10:34:35 -0800

    getting the preprocessing script together

---
+++ tonelocator/preprocess.py
@@ -0,0 +1,7 @@
+"""
+A function for implementing facial recognition
+- Options for cropping
+- Options for changing colorscale
+- Options for blur 
+"""
+
--- tonelocator/tests/test_detection.py
+++ tonelocator/tests/test_detection.py
@@ -32,3 +32,14 @@ class TestDetection(unittest.TestCase):
         test_no_picture_image_path = './data/practice_set/a_06.jpg'
         with self.assertRaises(ValueError):
             detection.complexion_detection(test_no_picture_image_path)
+    
+    def test_smoke(self):
+        """
+        Smoke test for functionality
+        Calls the detection on known picture in practice_set folder
+        Test is successful if no errors returned
+        """
+        smoke_image_path = './data/practice_set/a_01.jpg'
+        set1 = detection.complexion_detection(image_path = smoke_image_path, rounding_places = 1)
+        set2 = detection.complexion_detection(image_path = smoke_image_path, rounding_places = 3)
+        assert numpy.isclose(, 2)
