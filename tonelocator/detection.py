409d97594be0ddd90b357eb5e95144f79f7c00b8
Breon Haskett <breonh@uw.edu>
2022-12-08 12:18:28 -0800

    adding popular import convention for numpy and pandas

--- tonelocator/detection.py
+++ tonelocator/detection.py
@@ -3,12 +3,12 @@ Module defines the color detection function
 """
 
 import cv2
-import numpy
-import pandas
+import numpy as np
+import pandas as pd
 
 
-rgb_df = pandas.read_csv('https://raw.githubusercontent.com/583-dsdt-team/old-picture-complexion-detection/main/tonelocator/data/monk_scales_rgb.csv')
-bw_df = pandas.read_csv('https://raw.githubusercontent.com/583-dsdt-team/old-picture-complexion-detection/main/tonelocator/data/monk_scales_bw.csv')
+rgb_df = pd.read_csv('https://raw.githubusercontent.com/583-dsdt-team/old-picture-complexion-detection/main/tonelocator/data/monk_scales_rgb.csv')
+bw_df = pd.read_csv('https://raw.githubusercontent.com/583-dsdt-team/old-picture-complexion-detection/main/tonelocator/data/monk_scales_bw.csv')
 
 def complexion_detection(image_path, rounding_places=2, grayscale=False):
     """
@@ -27,16 +27,16 @@ def complexion_detection(image_path, rounding_places=2, grayscale=False):
 
     if grayscale == False:
         for index in range(rgb_df.shape[0]):
-            upper = numpy.uint8([rgb_df['upper_b'][index], rgb_df['upper_g'][index], rgb_df['upper_r'][index]])
-            lower = numpy.uint8([rgb_df['lower_b'][index], rgb_df['lower_g'][index], rgb_df['lower_r'][index]])
+            upper = np.uint8([rgb_df['upper_b'][index], rgb_df['upper_g'][index], rgb_df['upper_r'][index]])
+            lower = np.uint8([rgb_df['lower_b'][index], rgb_df['lower_g'][index], rgb_df['lower_r'][index]])
             mask = cv2.inRange(temp_image, lower, upper)
             cur_val = (mask > 0).mean()
             rounded_val = round(cur_val, rounding_places)
             filler.append(rounded_val)
     elif grayscale == True:
         for index in range(bw_df.shape[0]):
-            upper = numpy.uint8([bw_df['upper_b'][index], bw_df['upper_g'][index], bw_df['upper_r'][index]])
-            lower = numpy.uint8([bw_df['lower_b'][index], bw_df['lower_g'][index], bw_df['lower_r'][index]])
+            upper = np.uint8([bw_df['upper_b'][index], bw_df['upper_g'][index], bw_df['upper_r'][index]])
+            lower = np.uint8([bw_df['lower_b'][index], bw_df['lower_g'][index], bw_df['lower_r'][index]])
             mask = cv2.inRange(temp_image, lower, upper)
             cur_val = (mask > 0).mean()
             rounded_val = round(cur_val, rounding_places)
--- tonelocator/preprocess.py
+++ tonelocator/preprocess.py
@@ -5,3 +5,4 @@ A function for implementing facial recognition
 - Options for blur 
 """
 
+def face_detection(image_path, ):
