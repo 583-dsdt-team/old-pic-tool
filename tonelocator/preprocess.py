659b135970831b703add4495842ff1479024cde3
breon haskett <breonh@uw.edu>
2022-12-13 14:43:54 -0800

    linting preprocess

--- tonelocator/preprocess.py
+++ tonelocator/preprocess.py
@@ -17,7 +17,7 @@ def face_detection(origin_path, destination_path, keep_colors = True, gaussian_b
     Preprocessing of images for skin tone detection.
     """
     temp_picture = cv2.imread(origin_path)
-    face_detected = face_cascade.detectMultiScale(example_picture, 1.1, 4)
+    face_detected = face_cascade.detectMultiScale(temp_picture, 1.1, 4)
     for (x, y, w, h) in face_detected:
         cv2.rectangle(face_detected, (x, y), (x+w, y+h), (0, 0, 255), 2)
         face = temp_picture[y:y+15 + h, x+15:x +w]
