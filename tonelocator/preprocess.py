ed921bc760c1249d7433f870977691f0cec0c989
breon haskett <breonh@uw.edu>
2022-12-13 14:31:13 -0800

    preprocessor update

--- tonelocator/preprocess.py
+++ tonelocator/preprocess.py
@@ -2,7 +2,19 @@
 A function for implementing facial recognition
 - Options for cropping
 - Options for changing colorscale
-- Options for blur 
+- Options for blur
 """
 
-def face_detection(image_path, ):
+face_cascade = cv2.CascadeClassifier("/mnt/c/Users/breha/Documents/opencv-master/data/haarcascades/haarcascade_frontalface_default.xml")
+    
+def face_detection(origin_path, destination_path, keep_colors = True, gaussian_blur = False):
+    """
+    Preprocessing of images for skin tone detection.
+    """
+    temp_picture = cv2.imread(image_path)
+    face_detected = face_cascade.detectMultiScale(example_picture, 1.1, 4)
+    for (x, y, w, h) in face_detected:
+        cv2.rectangle(face_detected, (x, y), (x+w, y+h), (0, 0, 255), 2)
+        face = example_picture[y:y+15 + h, x+15:x +w]
+        cv2.imwrite(destination_path, face)
+
