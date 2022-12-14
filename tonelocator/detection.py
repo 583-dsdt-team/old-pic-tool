3b6b0fb50bb3ff696e41d9a65c7a0a2084e37ba3
breon haskett <breonh@uw.edu>
2022-12-14 10:17:30 -0800

    update preprocessor and light linting

--- tonelocator/detection.py
+++ tonelocator/detection.py
@@ -16,23 +16,22 @@ def complexion_detection(image_path, rounding_places=2, grayscale=False):
     Uses the inRange function in opencv
     Detects % of colors from ellis monk scale in image
     """
-
     filler = []
     path_check = os.path.isfile(image_path)
 
-    if path_check == False:
+    if path_check is False:
         raise ValueError('File does not exist at this location')
     else:
         pass
 
     temp_image = cv2.imread(image_path)
-    
+
     if temp_image is None:
         raise ValueError('Image file not read.')
     else:
         pass
 
-    if grayscale == False:
+    if grayscale is False:
         for index in range(rgb_df.shape[0]):
             upper = np.uint8([rgb_df['upper_b'][index], rgb_df['upper_g'][index], rgb_df['upper_r'][index]])
             lower = np.uint8([rgb_df['lower_b'][index], rgb_df['lower_g'][index], rgb_df['lower_r'][index]])
@@ -40,7 +39,7 @@ def complexion_detection(image_path, rounding_places=2, grayscale=False):
             cur_val = (mask > 0).mean()
             rounded_val = round(cur_val, rounding_places)
             filler.append(rounded_val)
-    elif grayscale == True:
+    elif grayscale is True:
         for index in range(bw_df.shape[0]):
             upper = np.uint8([bw_df['upper_b'][index], bw_df['upper_g'][index], bw_df['upper_r'][index]])
             lower = np.uint8([bw_df['lower_b'][index], bw_df['lower_g'][index], bw_df['lower_r'][index]])
--- tonelocator/preprocess.py
+++ tonelocator/preprocess.py
@@ -5,20 +5,32 @@ A function for implementing facial recognition
 - Options for blur
 """
 import cv2
-import numpy
+import numpy as np
 import os
-import pandas
+import pandas as pd
 
 
 face_cascade = cv2.CascadeClassifier("./tonelocator/data/haarcascades/haarcascade_frontalface_default.xml")
     
-def face_detection(origin_path, destination_path, keep_colors = True, gaussian_blur = False):
+def face_crop(image_path, to_gray=False, gaussian_blur=False):
     """
-    Preprocessing of images for skin tone detection.
+    Preprocessing of images for skin tone detection:
+    a face detector and crop
+    Returns image as an array
     """
-    temp_picture = cv2.imread(origin_path)
+    temp_picture = cv2.imread(image_path)
     face_detected = face_cascade.detectMultiScale(temp_picture, 1.1, 4)
     for (x, y, w, h) in face_detected:
         cv2.rectangle(face_detected, (x, y), (x+w, y+h), (0, 0, 255), 2)
         face = temp_picture[y:y+15 + h, x+15:x +w]
-        cv2.imwrite(destination_path, face)
+        
+    if to_gray is True:
+        face = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
+    else:
+        pass
+
+    if gaussian_blur is True:
+        face = cv2.blur(face, (5,5))
+    else:
+        pass
+    return face
