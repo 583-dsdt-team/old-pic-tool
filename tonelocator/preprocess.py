013e4bfc2b26310f8793523d8fa5a113fcb24854
breon haskett <breonh@uw.edu>
2022-12-14 15:08:33 -0800

    added a test for nondetection of faces

--- tonelocator/preprocess.py
+++ tonelocator/preprocess.py
@@ -4,26 +4,43 @@ A function for implementing facial recognition
 - Options for changing colorscale
 - Options for blur
 """
+import os
 import cv2
 import numpy as np
-import os
 import pandas as pd
 
 
 face_cascade = cv2.CascadeClassifier("./tonelocator/data/haarcascade_frontalface_default.xml")
-    
+
 def face_crop(image_path, to_gray=False, gaussian_blur=False):
     """
     Preprocessing of images for skin tone detection:
     a face detector and crop
     Returns image as an array
     """
-    temp_picture = cv2.imread(image_path)
-    face_detected = face_cascade.detectMultiScale(temp_picture, 1.1, 4)
-    for (x, y, w, h) in face_detected:
-        cv2.rectangle(face_detected, (x, y), (x+w, y+h), (0, 0, 255), 2)
-        face = temp_picture[y:y+15 + h, x+15:x +w]
-        
+    path_check = os.path.isfile(image_path)
+
+    if path_check is False:
+        raise ValueError('File does not exist at this location')
+    else:
+        pass
+
+    temp_image = cv2.imread(image_path)
+
+    if temp_image is None:
+        raise ValueError('Image file not read.')
+    else:
+        pass
+
+    face_detected = face_cascade.detectMultiScale(temp_image, 1.1, 4)
+
+    if isinstance(face_detected, np.ndarray):
+        for (x_dim, y_dim, width, height) in face_detected:
+            cv2.rectangle(face_detected, (x_dim, y_dim), (x_dim+width, y_dim+height), (0, 0, 255), 2)
+            face = temp_image[y_dim:y_dim+15 + height, x_dim+15:x_dim +width]
+    else:
+        raise TypeError('Face not properly detected')
+
     if to_gray is True:
         face = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
     else:
--- tonelocator/tests/test_preprocess.py
+++ tonelocator/tests/test_preprocess.py
@@ -22,3 +22,23 @@ class TestPreprocess(unittest.TestCase):
         """
         smoke_image_path = './tonelocator/data/practice_set/a_01.jpg'
         preprocess.face_crop(image_path = smoke_image_path)
+    
+    def test_no_picture(self):
+        """
+        One shot test for missing file
+        Calls the detection on picture not in practice_set folder
+        Test is successful if error is caught
+        """
+        test_no_picture_image_path = './tonelocator/data/practice_set/a_06.jpg'
+        with self.assertRaises(ValueError):
+            preprocess.face_crop(test_no_picture_image_path)
+
+    def test_no_face_detected(self):
+        """
+        One shot test for missing file
+        Calls the detection on blank picture
+        Test is successful if error is caught
+        """
+        test_blank_picture_image_path = './tonelocator/data/test/blank_picture_test.JPG'
+        with self.assertRaises(TypeError):
+            preprocess.face_crop(test_blank_picture_image_path)
